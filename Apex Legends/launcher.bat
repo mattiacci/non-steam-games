551b7e01458e01c87807b1398705680e2c230494
Michael Mattiacci <mattiacci@gmail.com>
2019-02-08 23:28:41 -0800

    Add support for Apex Legends

---
+++ Apex Legends/launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+
+set COMMAND="C:\Program Files (x86)\Origin Games\Apex\r5apex.exe"
+set EXES_TO_WATCH="r5apex.exe"
+set LAUNCHER_EXE="Origin.exe"
+
+
+call ..\src\launcher.bat

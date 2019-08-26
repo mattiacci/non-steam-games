01b88b50f8623857ed9affd762b586e7553245eb
Michael Mattiacci <mattiacci@gmail.com>
2019-08-25 21:50:34 -0700

    Add support for Steep, and Minecraft (Java)

---
+++ Minecraft (Java)/launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+
+set COMMAND="C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
+set EXES_TO_WATCH="MinecraftLauncher.exe"
+set LAUNCHER_EXE=""
+
+
+call ..\src\launcher.bat
---
+++ Steep/launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+
+set COMMAND="uplay://launch/3279/0"
+set EXES_TO_WATCH="steep.exe"
+set LAUNCHER_EXE="upc.exe"
+
+
+call ..\src\launcher.bat
